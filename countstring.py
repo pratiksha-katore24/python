6f268408b5540fbd6edd159eaf86c043fb8ff264
pratiksha-katore24 <158153399+pratiksha-katore24@users.noreply.github.com>
2024-09-30 15:01:08 +0530

    Add files via upload

---
+++ countstring.py
@@ -0,0 +1,9 @@
+dict = {}
+for ch in check_string:
+    if ch in dict:
+        dict[ch] += 1
+        else:
+            dict[ch] = 1
+            for key in dict:
+                print(key, "-" , dict[key])
+            
