a0369042ffe41d2d50fee51069f46f83af8612c3
pratiksha-katore24 <158153399+pratiksha-katore24@users.noreply.github.com>
2024-10-01 15:08:22 +0530

    Add files via upload

---
+++ queue.py
@@ -0,0 +1,18 @@
+q=[]
+q.append(10)
+print("Initial Queue is:",q)
+q.append(100)
+print("Initial Queue is:",q)
+q.append(1000)
+print("Initial Queue is:",q)
+q.append(10000)
+print("Initial Queue is:",q)
+
+print('{} is out from queue ! '.format(q.pop(0)))
+print("After Removing elements:",q)
+print('{} is out from queue ! '.format(q.pop(0)))
+print("After Removing elements:",q)
+print('{} is out from queue ! '.format(q.pop(0)))
+print("After Removing elements:",q)
+
+
